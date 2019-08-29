03a1a47944d17020949292eee2dfafe95317dec3
kakha <kaxgel11@gmail.com>
2019-08-29 12:24:37 +0400

    params generator

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import sys
 import numpy
+from itertools import product
 
 app = Flask(__name__)
 
@@ -16,14 +17,36 @@ if __name__ == '__main__':
 
 @app.route('/start')
 def start():
-    # for item in productMap['iphoneXs']['Excellent']:
-    #     print(item)
-    list = numpy.stack(productMap['iphoneXs']['Excellent'], axis=0)
-    return 'Hello World!'
+    product = 'iphoneXS'
+    conditon = 'Poor'
+    result = getParams(product, conditon)
+    for params in result:
+        print(params)
+
+    return result.__str__()
+
+
+def getParams(product, conditon):
+    result = []
+    for item0 in productMap[product][conditon][0]:
+        for item1 in productMap[product][conditon][1]:
+            for item2 in productMap[product][conditon][2]:
+                for item3 in productMap[product][conditon][3]:
+                    for item4 in productMap[product][conditon][4]:
+                        for item5 in productMap[product][conditon][5]:
+                            for item6 in productMap[product][conditon][6]:
+                                for item7 in productMap[product][conditon][7]:
+                                    for item8 in productMap[product][conditon][8]:
+                                        for item9 in productMap[product][conditon][9]:
+                                            for item10 in productMap[product][conditon][10]:
+                                                result.append(
+                                                    [item0, item1, item2, item3, item4, item5, item6, item7, item8,
+                                                     item9, item10])
+    return result
 
 
 productMap = {
-    'iphoneXs': {
+    'iphoneXS': {
         'Excellent': [[7399], [7396, 3987, 2023], [2014], [2452, 2902, 2903], [2072], [2026], [2100], [2125], [2118],
                       [2114], [2067]],
         'Good': [[7399], [7396, 3987, 2023], [2014], [2452, 2902, 2903], [2072], [2026], [2100], [2125, 2126],
